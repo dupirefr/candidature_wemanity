9bd15df9fe0c718a6a6564191177d9bc302e8c45
fdpro <dupire.francois+pro@gmail.com>
2020-10-15 14:11:54 +0200

    * Improved test, verifying the whole item and not just its name

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,25 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
-
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void updateQuality_regularItems() {
+        Item[] items = new Item[] {
+          new Item("already_degraded_item", 0, 0)
+        };
+
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+
+        assertItemsEquals(new Item("already_degraded_item", -1, 0), app.items[0]);
     }
 
+    void assertItemsEquals(Item expected, Item actual) {
+        assertThat(actual.name).describedAs("Item name should be %s, but was %s", expected.name, actual.name).isEqualTo(expected.name);
+        assertThat(actual.sellIn).describedAs("Item sell-in should be %d, but was %d", expected.sellIn, actual.sellIn).isEqualTo(expected.sellIn);
+        assertThat(actual.quality).describedAs("Item quality should be %d, but was %d", actual.quality).isEqualTo(expected.quality);
+    }
 }
