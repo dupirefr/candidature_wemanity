e2d23838413c13060e6f0489948b62137db04ab6
fdpro <dupire.francois+pro@gmail.com>
2020-10-15 14:39:32 +0200

    * Refactored test for further scenarios addition

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,60 +1,72 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
-    @Test
-    void updateQuality_regularItems() {
-        Item[] itemsInTheStore = new Item[]{
-          new Item("Still valid, undegraded item", 1, 2),
-          new Item("Still valid, degraded item", 1, 0),
-          new Item("Not valid anymore, undegraded item", 0, 2),
-          new Item("Not valid anymore, degraded item", 0, 0)
-        };
-
+    @DisplayName("Update quality rules test for:")
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("dataSet")
+    void updateQuality(String description, Item[] itemsInTheStore, List<Item> expectedItems) {
         GildedRose app = new GildedRose(itemsInTheStore);
         app.updateQuality();
-
-        List<Item> expectedItems = Arrays.asList(
-          new Item("Still valid, undegraded item", 0, 1),
-          new Item("Still valid, degraded item", 0, 0),
-          new Item("Not valid anymore, undegraded item", -1, 0),
-          new Item("Not valid anymore, degraded item", -1, 0)
-        );
 
         assertItemsEqual(expectedItems, Arrays.asList(itemsInTheStore));
     }
 
-    @Test
-    void updateQuality_agedBrie() {
-        Item[] itemsInTheStore = new Item[]{
-          new Item("Aged Brie", 1, 0),
-          new Item("Aged Brie", 0, 0),
-          new Item("Aged Brie", 0, 50)
-        };
-
-        GildedRose app = new GildedRose(itemsInTheStore);
-        app.updateQuality();
-
-        List<Item> expectedItems = Arrays.asList(
-          new Item("Aged Brie", 0, 1),
-          new Item("Aged Brie", -1, 2),
-          new Item("Aged Brie", -1, 50)
+    private static Stream<Arguments> dataSet() {
+        return Stream.of(
+          regularItemsDataSet(),
+          agedBrieDataSet()
         );
+    }
 
-        assertItemsEqual(expectedItems, Arrays.asList(itemsInTheStore));
+    private static Arguments regularItemsDataSet() {
+        return Arguments.arguments(
+          "Regular items",
+          new Item[]{
+            new Item("Still valid, undegraded item", 1, 2),
+            new Item("Still valid, degraded item", 1, 0),
+            new Item("Not valid anymore, undegraded item", 0, 2),
+            new Item("Not valid anymore, degraded item", 0, 0)
+          },
+          Arrays.asList(
+            new Item("Still valid, undegraded item", 0, 1),
+            new Item("Still valid, degraded item", 0, 0),
+            new Item("Not valid anymore, undegraded item", -1, 0),
+            new Item("Not valid anymore, degraded item", -1, 0)
+          )
+        );
+    }
+
+    private static Arguments agedBrieDataSet() {
+        return Arguments.arguments(
+          "Aged Brie",
+          new Item[]{
+            new Item("Aged Brie", 1, 0),
+            new Item("Aged Brie", 0, 0),
+            new Item("Aged Brie", 0, 50)
+          },
+          Arrays.asList(
+            new Item("Aged Brie", 0, 1),
+            new Item("Aged Brie", -1, 2),
+            new Item("Aged Brie", -1, 50)
+          )
+        );
     }
 
     void assertItemsEqual(List<Item> expected, List<Item> actual) {
         assertThat(actual).describedAs("There should be the same number of actual than expected items").hasSameSizeAs(expected);
         for (int i = 0; i < actual.size(); i++) {
-           assertItemsEqual(expected.get(i), actual.get(i));
+            assertItemsEqual(expected.get(i), actual.get(i));
         }
     }
 
