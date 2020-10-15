08dd385406c9d75e27e5154ae96f51e5aaf02ff7
fdpro <dupire.francois+pro@gmail.com>
2020-10-15 14:27:57 +0200

    * Increased test coverage: tested regular items sell-in/quality decrease rules

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,25 +2,53 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
     @Test
     void updateQuality_regularItems() {
-        Item[] items = new Item[] {
-          new Item("already_degraded_item", 0, 0)
+        Item[] itemsInTheStore = new Item[]{
+          new Item("Still valid, undegraded item", 1, 2),
+          new Item("Still valid, degraded item", 1, 0),
+          new Item("Not valid anymore, undegraded item", 0, 2),
+          new Item("Not valid anymore, degraded item", 0, 0)
         };
 
-        GildedRose app = new GildedRose(items);
+        GildedRose app = new GildedRose(itemsInTheStore);
         app.updateQuality();
 
-        assertItemsEquals(new Item("already_degraded_item", -1, 0), app.items[0]);
+        List<Item> expectedItems = Arrays.asList(
+          new Item("Still valid, undegraded item", 0, 1),
+          new Item("Still valid, degraded item", 0, 0),
+          new Item("Not valid anymore, undegraded item", -1, 0),
+          new Item("Not valid anymore, degraded item", -1, 0)
+        );
+
+        assertItemsEqual(expectedItems, Arrays.asList(itemsInTheStore));
     }
 
-    void assertItemsEquals(Item expected, Item actual) {
-        assertThat(actual.name).describedAs("Item name should be %s, but was %s", expected.name, actual.name).isEqualTo(expected.name);
-        assertThat(actual.sellIn).describedAs("Item sell-in should be %d, but was %d", expected.sellIn, actual.sellIn).isEqualTo(expected.sellIn);
-        assertThat(actual.quality).describedAs("Item quality should be %d, but was %d", actual.quality).isEqualTo(expected.quality);
+    void assertItemsEqual(List<Item> expected, List<Item> actual) {
+        assertThat(actual).describedAs("There should be the same number of actual than expected items").hasSameSizeAs(expected);
+        for (int i = 0; i < actual.size(); i++) {
+           assertItemsEqual(expected.get(i), actual.get(i));
+        }
+    }
+
+    void assertItemsEqual(Item expected, Item actual) {
+        assertThat(actual.name)
+          .describedAs("Item '%s' name should be %s, but was %s", actual.name, expected.name, actual.name)
+          .isEqualTo(expected.name);
+
+        assertThat(actual.sellIn)
+          .describedAs("Item '%s' sell-in should be %d, but was %d", actual.name, expected.sellIn, actual.sellIn)
+          .isEqualTo(expected.sellIn);
+
+        assertThat(actual.quality)
+          .describedAs("Item '%s' quality should be %d, but was %d", actual.name, expected.quality, actual.quality)
+          .isEqualTo(expected.quality);
     }
 }
