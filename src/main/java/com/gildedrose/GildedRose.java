5f1a6c901d5e00f4ff68b7f2ea4cc1269b5612da
fdpro <dupire.francois+pro@gmail.com>
2020-10-15 14:49:56 +0200

    * Extracted 'Backstage passes' check

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,7 +10,7 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             if (!isAgedBrie(item)
-              && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+              && !isBackstagePasses(item)) {
                 if (item.quality > 0) {
                     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                         item.quality = item.quality - 1;
@@ -20,7 +20,7 @@ class GildedRose {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (isBackstagePasses(item)) {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1;
@@ -42,7 +42,7 @@ class GildedRose {
 
             if (item.sellIn < 0) {
                 if (!isAgedBrie(item)) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (!isBackstagePasses(item)) {
                         if (item.quality > 0) {
                             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                                 item.quality = item.quality - 1;
@@ -62,5 +62,9 @@ class GildedRose {
 
     private boolean isAgedBrie(Item item) {
         return item.name.equals("Aged Brie");
+    }
+
+    private boolean isBackstagePasses(Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
 }
